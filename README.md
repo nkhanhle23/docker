d4432bf855a4d9c849430d63ac9a8c925e26a505
nkhanhle <nguyenkhanhle98@gmail.com>
2023-05-07 14:28:03 +0200

    "Customized Website."

---
+++ README.md
@@ -0,0 +1,3 @@
+Introduction to Docker
+Points that are not accomplished: 
+1. Could not add password to the Redis database, connection error
--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 import time
 import redis
 from flask import Flask, render_template
-import os
-from dotenv import load_dotenv
+#import os
+#from dotenv import load_dotenv
+import pandas as pd
+import matplotlib.pyplot as plt
 
-load_dotenv() 
-cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
+#load_dotenv() 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -24,5 +27,20 @@ def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
 
+@app.route('/titanic')
+def titanic():
+    df = pd.read_csv("titanic.csv")
+    table = df.head(5).to_html()
+    gender_counts = df.groupby(['Sex', 'Survived']).size().unstack()
+    plot = gender_counts.plot(kind='bar')
+    plot.set_xlabel("Gender")
+    plot.set_ylabel("Number of Survivors")
+    plot.legend(["Did Not Survive", "Survived"])
+    plt.title("Survival by Gender")
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    chart = plot.get_figure()
+    chart.savefig('static/chart.png')
+    return render_template('titanic.html', table=table, chart_url = url_for("static",filename="chart.png"))
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
